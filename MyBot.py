6a3731afc1bec49f173c7a058c915e9322b0b77c
Taylor Shin <t2shin@ucsd.edu>
2017-02-01 13:55:19 -0800

    improved bot by telling all zero strength pieces to stay still

--- MyBot.py
+++ MyBot.py
@@ -6,7 +6,13 @@ import random
 myID, game_map = hlt.get_init()
 hlt.send_init("MyPythonBot")
 
+def assign_move(square):
+    if square.strength == 0:
+        return Move(square, STILL)
+    else:
+        return Move(square, random.choice((NORTH, EAST, SOUTH, WEST)))
+
 while True:
     game_map.get_frame()
-    moves = [Move(square, random.choice((NORTH, EAST, SOUTH, WEST, STILL))) for square in game_map if square.owner == myID]
+    moves = [assign_move(square) for square in game_map if square.owner == myID]
     hlt.send_frame(moves)
