716477b52bb023bbacd357fb3885ed809d27879c
Taylor Shin <t2shin@ucsd.edu>
2017-02-12 14:19:28 -0800

    added logging but not doing this programming challenge anymore

--- MyBot.py
+++ MyBot.py
@@ -1,7 +1,9 @@
 import hlt
 from hlt import NORTH, EAST, SOUTH, WEST, STILL, Move, Square
 import random
+import logging
 
+logging.basicConfig(filename='output.log',level=logging.DEBUG)
 
 myID, game_map = hlt.get_init()
 hlt.send_init("MyPythonBot")
@@ -15,4 +17,5 @@ def assign_move(square):
 while True:
     game_map.get_frame()
     moves = [assign_move(square) for square in game_map if square.owner == myID]
+    logging.debug(moves)
     hlt.send_frame(moves)
